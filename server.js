62f99791cb7f6d2a37c0b530fde970850fb9df17
toledomg <toledomg@yahoo.com.br>
2024-09-13 11:41:42 -0300

    bugFix: add dotenv in api port

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const jsonServer = require("json-server");
-const auth = require("json-server-auth");
-const cors = require("cors");
+require('dotenv').config();
+
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const cors = require('cors');
 const port = process.env.PORT || 3001;
 
 const app = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 
 app.db = router.db;
 
 const rules = auth.rewriter({
   users: 600,
-  products: 660
+  products: 660,
 });
 
 app.use(cors());
@@ -19,4 +21,4 @@ app.use(auth);
 app.use(router);
 app.listen(port);
 
-console.log("Server is running on port:", port);
+console.log('Server is running on port:', port);
